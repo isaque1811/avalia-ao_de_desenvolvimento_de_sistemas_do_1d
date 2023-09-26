118ca4511eac636c206643ea0184ca86e510e1f9
isaque1811 <144157112+isaque1811@users.noreply.github.com>
2023-09-26 14:40:03 -0300

    v.2 da avaliaçao1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,5 +1,5 @@
-num1=(input("digite seu nome"))
+num1=(input("digite seu nome"))#local em que vai ser digitado seu nome
 num2=int(input("digite o ano atual"))
 num3=int(input("digite o ano em que voce nasceu"))
-total=num1-num2
+total=num1-num2#subtraçao entre o ano e a data
 print=(input("ola",num1,"sua idade é",total))
