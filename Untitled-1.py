8567936187b055200b4293caddd46273e633fd2d
jussara2 <freitasjussara887@gmail.com>
2023-09-26 14:28:55 -0300

    avaliaçao 1d
    logica de programaçao do 1d

---
+++ Untitled-1.py
@@ -0,0 +1,5 @@
+num1=(input("digite seu nome"))
+num2=int(input("digite o ano atual"))
+num3=int(input("digite o ano em que voce nasceu"))
+total=num1-num2
+print=(input("ola",num1,"sua idade é",total))
